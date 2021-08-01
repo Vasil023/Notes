5043ce6e7d1b73b0773bce29ab65fcaee86c350a
Vasil <voloshin017@gmail.com>
2021-08-01 22:44:05 +0300

    added component notes

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,10 @@
         :note="note"
         @addNote="addNote" />
 
-        <div class="notes">
-          <div class="note" v-for="(note, index) in notes" :key="index">
-            <div class="note-header">
-              <p>{{ note.title }}</p>
-            </div> 
-            <div class="note-body">
-              <p>{{ note.descr }}</p>
-              <span>{{ note.date }}</span>
-            </div>
-          </div>
-        </div>
+        <notes 
+        :notes="notes"/>
+
+       
        </div>
      </section>
    </div>
@@ -31,9 +24,10 @@
 <script>
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
+import notes from '@/components/Notes.vue'
 export default {
   components: {
-    message, newNote
+    message, newNote, notes
   },
   data () {
     return {
